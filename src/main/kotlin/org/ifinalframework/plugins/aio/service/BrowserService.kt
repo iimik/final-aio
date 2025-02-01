1d4039260752e8c9a1066caf1bf1b757ce14209c
iimik <iimik@qq.com>
2025-02-01 17:18:24 +0800

    docs: BrowserService

--- src/main/kotlin/org/ifinalframework/plugins/aio/service/BrowserService.kt
+++ src/main/kotlin/org/ifinalframework/plugins/aio/service/BrowserService.kt
@@ -4,12 +4,15 @@ import org.ifinalframework.plugins.aio.application.annotation.ImplementedBy
 
 
 /**
- * BrowserService
+ * 浏览器服务
  *
  * @author iimik
  * @since 0.0.1
  **/
 @ImplementedBy(DefaultBrowserService::class)
 interface BrowserService {
+    /**
+     * 打开指定的URL
+     */
     fun open(url: String)
 }
