19e2b4da74ff09abeed99e1694c907b84472e7fa
iimik <iimik@qq.com>
2025-02-04 17:33:26 +0800

    feature: .env config file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,15 +7,14 @@
 ### 新增
 
 - Api 管理
-  - 添加Api方法Mark
-  - down行标记，并可快速在编辑器中打开进行编辑。(#14)
-  - 添加Api方法View行标记，可快速在浏览器中打开对应URL。(#16)
+  - 🔥添加Api方法Markdown行标记，并可快速在编辑器中打开进行编辑。(#14)
+  - 🔥添加Api方法View行标记，可快速在浏览器中打开对应URL。(#16)
 - Issue 管理
-  - Issue行标记支持Markdown，格式`(#issue)` (#18)
+  - 🔥Issue行标记支持Markdown，格式`(#issue)` (#18)
 
 ## [0.0.1] - 2025-01-27
 
 ### 新增
 
 - Issue 管理
-  - 添加Issue行标记，并可在浏览器中快速打开，支持Git Issue和Jira。(#10)
+  - 🔥添加Issue行标记，并可在浏览器中快速打开，支持Git Issue和Jira。(#10)
--- src/main/kotlin/org/ifinalframework/plugins/aio/application/ElementEnvironment.kt
+++ src/main/kotlin/org/ifinalframework/plugins/aio/application/ElementEnvironment.kt
@@ -2,7 +2,9 @@ package org.ifinalframework.plugins.aio.application;
 
 import com.intellij.psi.PsiElement
 import org.ifinalframework.plugins.aio.R
+import org.springframework.boot.env.PropertiesPropertySourceLoader
 import org.springframework.boot.env.PropertySourceLoader
+import org.springframework.core.env.MutablePropertySources
 import org.springframework.core.env.StandardEnvironment
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
@@ -14,10 +16,24 @@ import java.util.*
 /**
  * ElementEnvironment
  *
+ * - `.final.properties`
+ * - `.final.local.properties`
+ * - `.final.yml`
+ * - `.final.local.yml`
+ * - `.final.yaml`
+ * - `.final.local.yaml`
+ * - `.env`
+ * - `.env.local`
+ *
+ *
  * @author iimik
  * @since 0.0.1
  **/
 class ElementEnvironment : StandardEnvironment(), ElementPropertySourcesLoader {
+
+    private val configFileNames = arrayOf(".final", ".final.local")
+    private val envConfigFileName = arrayOf(".env", ".env.local")
+
     override fun load(classLoader: ClassLoader, element: PsiElement) {
         val propertySources = propertySources
 
@@ -32,26 +48,42 @@ class ElementEnvironment : StandardEnvironment(), ElementPropertySourcesLoader {
 
         for (configPath in configPaths) {
             for (propertySourceLoader in propertySourceLoaders) {
-                for (extension in propertySourceLoader.fileExtensions) {
-                    try {
-                        val file = "$configPath/$CONFIG_FILE_NAME$extension"
-                        val urlResource: Resource = FileSystemResource(file)
-                        if (urlResource.exists()) {
-                            logger.info("found config file: " +  file.substring(basePath.length + 1))
-                            val sources = propertySourceLoader.load(file, urlResource)
-                            for (source in sources) {
-                                propertySources.addFirst(source)
-                            }
-                        }
-                    } catch (e: IOException) {
-                        throw RuntimeException(e)
+                if (propertySourceLoader is PropertiesPropertySourceLoader) {
+                    for (fileName in envConfigFileName) {
+                        val file = "$configPath/$fileName"
+                        doLoad(file, basePath, propertySourceLoader, propertySources)
                     }
                 }
+
+                for (extension in propertySourceLoader.fileExtensions) {
+                    for (fileName in configFileNames) {
+                        val file = "$configPath/${fileName.trimEnd('.')}.$extension"
+                        doLoad(file, basePath, propertySourceLoader, propertySources)
+                    }
+                }
+
             }
         }
     }
 
-    companion object {
-        private const val CONFIG_FILE_NAME = ".final."
+    private fun doLoad(
+        file: String,
+        basePath: String,
+        propertySourceLoader: PropertySourceLoader,
+        propertySources: MutablePropertySources
+    ) {
+        try {
+            val urlResource: Resource = FileSystemResource(file)
+            if (urlResource.exists()) {
+                logger.info("found config file: " + file.substring(basePath.length + 1))
+                val sources = propertySourceLoader.load(file, urlResource)
+                for (source in sources) {
+                    propertySources.addFirst(source)
+                }
+            }
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
     }
+
 }
