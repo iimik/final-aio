377cfa194a243f156ab1a70d31d3facc9c20e799
iimik <iimik@qq.com>
2025-02-08 15:49:43 +0800

    feat: update KtStringTemplateExpression.plainContent (#21)

--- src/main/kotlin/org/ifinalframework/plugins/aio/jvm/kotlin/KotlinExpressionResolver.kt
+++ src/main/kotlin/org/ifinalframework/plugins/aio/jvm/kotlin/KotlinExpressionResolver.kt
@@ -1,22 +1,22 @@
 package org.ifinalframework.plugins.aio.jvm.kotlin;
 
 import org.ifinalframework.plugins.aio.jvm.ExpressionResolver
-import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.psi.KtCollectionLiteralExpression
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.kotlin.psi.psiUtil.plainContent
 
 
 /**
  * KotlinExpressionResolver
- *
+ * @issue 21
  * @author iimik
  * @since 0.0.2
  **/
 class KotlinExpressionResolver : ExpressionResolver<KtExpression> {
     override fun resolve(expression: KtExpression): Any? {
         return when (expression) {
-            is KtStringTemplateExpression -> expression.text.trim('"')
+            is KtStringTemplateExpression -> expression.plainContent
             is KtCollectionLiteralExpression -> expression.innerExpressions.map { resolve(it) }.toList()
             else -> expression
         }
