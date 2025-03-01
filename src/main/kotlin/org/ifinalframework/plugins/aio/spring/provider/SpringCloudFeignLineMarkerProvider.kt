f8bbe31c64dd1c63c7768e6746b864504481518e
iimik <iimik@qq.com>
2025-03-01 19:51:49 +0800

    feat: try-catch ProcessCanceledException for SpringCloudFeignLineMarkerProvider

--- src/main/kotlin/org/ifinalframework/plugins/aio/spring/provider/SpringCloudFeignLineMarkerProvider.kt
+++ src/main/kotlin/org/ifinalframework/plugins/aio/spring/provider/SpringCloudFeignLineMarkerProvider.kt
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiModifier
@@ -37,98 +38,104 @@ class SpringCloudFeignLineMarkerProvider : RelatedItemLineMarkerProvider() {
     private val feignTooltip = I18N.message("Spring.SpringCloudFeignLineMarkerProvider.feign.tooltip")
 
     override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
-        val u = element.toUElement() ?: return
-        if (u is UIdentifier && u.uastParent is UMethod) {
-            val uMethod = u.uastParent as UMethod
-            val uClass = uMethod.getContainingUClass() ?: return
 
-            val project = element.project
-            val psiService = project.getService(PsiService::class.java)
-            val apiMethodService = service<ApiMethodService>()
+        try {
 
-            if (uClass.isInterface && uClass.hasAnnotation(SpringAnnotations.FEIGN_CLIENT)) {
-                // @FeignClient
-                val apiMarker = apiMethodService.getApiMarker(element.parent) ?: return
+            val u = element.toUElement() ?: return
+            if (u is UIdentifier && u.uastParent is UMethod) {
+                val uMethod = u.uastParent as UMethod
+                val uClass = uMethod.getContainingUClass() ?: return
+
+                val project = element.project
+                val psiService = project.getService(PsiService::class.java)
+                val apiMethodService = service<ApiMethodService>()
+
+                if (uClass.isInterface && uClass.hasAnnotation(SpringAnnotations.FEIGN_CLIENT)) {
+                    // @FeignClient
+                    val apiMarker = apiMethodService.getApiMarker(element.parent) ?: return
 
 //                val controllers = psiService.findAllClasses(ClassFilter { psiClass ->
 //                    !psiClass.isInterface && psiClass.hasAnnotation(SpringAnnotations.REQUEST_MAPPING) && !psiClass.hasModifierProperty(PsiModifier.ABSTRACT)
 //                })
 
-                val controllers = jvmFileExtensions.flatMap {
-                    FilenameIndex.getAllFilesByExt(project, it)
-                        .asSequence()
-                        .filter { f -> f.name.endsWith("Controller.$it") }
-                        .mapNotNull { f -> f.toPsiFile(project) }
-                        .flatMap { f ->
-                            PsiTreeUtil.findChildrenOfAnyType(f, PsiClass::class.java, KtClass::class.java)
-                                .filter { clazz ->
-                                    val toUElement = clazz.toUElement()
-                                    return@filter if (toUElement is UClass) {
-                                        return@filter !toUElement.hasModifierProperty(PsiModifier.ABSTRACT)
-                                                && toUElement.hasAnnotation(SpringAnnotations.REQUEST_MAPPING)
-                                                && toUElement.name != null && toUElement.name!!.endsWith("Controller")
-                                    } else false
-                                }
-                                .mapNotNull { f -> f.kotlinFqName?.asString() }
-                        }
-                        .mapNotNull { name -> psiService.findClass(name) }
-                }
-
-                val methods = controllers.stream().flatMap { c -> c.allMethods.stream() }
-                    .filter { method ->
-                        val marker = apiMethodService.getApiMarker(method)
-                        marker != null && marker.contains(apiMarker)
+                    val controllers = jvmFileExtensions.flatMap {
+                        FilenameIndex.getAllFilesByExt(project, it)
+                            .asSequence()
+                            .filter { f -> f.name.endsWith("Controller.$it") }
+                            .mapNotNull { f -> f.toPsiFile(project) }
+                            .flatMap { f ->
+                                PsiTreeUtil.findChildrenOfAnyType(f, PsiClass::class.java, KtClass::class.java)
+                                    .filter { clazz ->
+                                        val toUElement = clazz.toUElement()
+                                        return@filter if (toUElement is UClass) {
+                                            return@filter !toUElement.hasModifierProperty(PsiModifier.ABSTRACT)
+                                                    && toUElement.hasAnnotation(SpringAnnotations.REQUEST_MAPPING)
+                                                    && toUElement.name != null && toUElement.name!!.endsWith("Controller")
+                                        } else false
+                                    }
+                                    .mapNotNull { f -> f.kotlinFqName?.asString() }
+                            }
+                            .mapNotNull { name -> psiService.findClass(name) }
                     }
-                    .toList()
 
-                if (methods.isNotEmpty()) {
-                    val icon = AllIcons.Spring.MVC
-                    val navigationGutterIconBuilder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(icon)
-                    navigationGutterIconBuilder.setTooltipText(feignTooltip)
-                    navigationGutterIconBuilder.setAlignment(GutterIconRenderer.Alignment.CENTER)
-                    navigationGutterIconBuilder.setTargets(methods)
-                    result.add(navigationGutterIconBuilder.createLineMarkerInfo(element))
-                }
-
-            } else if (uClass.hasAnnotation(SpringAnnotations.REQUEST_MAPPING) && !uClass.hasModifierProperty(PsiModifier.ABSTRACT)) {
-                val apiMarker = apiMethodService.getApiMarker(element.parent) ?: return
-                val controllers = jvmFileExtensions.flatMap {
-                    FilenameIndex.getAllFilesByExt(project, it)
-                        .asSequence()
-                        .filter { f -> f.name.endsWith("Client.$it") }
-                        .mapNotNull { f -> f.toPsiFile(project) }
-                        .flatMap { f ->
-                            PsiTreeUtil.findChildrenOfAnyType(f, PsiClass::class.java, KtClass::class.java)
-                                .filter { clazz ->
-                                    val toUElement = clazz.toUElement()
-                                    return@filter if (toUElement is UClass) {
-                                        return@filter toUElement.isInterface
-                                                && toUElement.hasAnnotation(SpringAnnotations.FEIGN_CLIENT)
-                                                && toUElement.name != null && toUElement.name!!.endsWith("Client")
-                                    } else false
-                                }
-                                .mapNotNull { f -> f.kotlinFqName?.asString() }
+                    val methods = controllers.stream().flatMap { c -> c.allMethods.stream() }
+                        .filter { method ->
+                            val marker = apiMethodService.getApiMarker(method)
+                            marker != null && marker.contains(apiMarker)
                         }
-                        .mapNotNull { name -> psiService.findClass(name) }
-                }
+                        .toList()
 
-                val methods = controllers.stream().flatMap { c -> c.allMethods.stream() }
-                    .filter { method ->
-                        val marker = apiMethodService.getApiMarker(method)
-                        marker != null && marker.contains(apiMarker)
+                    if (methods.isNotEmpty()) {
+                        val icon = AllIcons.Spring.MVC
+                        val navigationGutterIconBuilder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(icon)
+                        navigationGutterIconBuilder.setTooltipText(feignTooltip)
+                        navigationGutterIconBuilder.setAlignment(GutterIconRenderer.Alignment.CENTER)
+                        navigationGutterIconBuilder.setTargets(methods)
+                        result.add(navigationGutterIconBuilder.createLineMarkerInfo(element))
                     }
-                    .toList()
 
-                if (methods.isNotEmpty()) {
-                    val icon = AllIcons.Spring.CLOUD
-                    val navigationGutterIconBuilder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(icon)
-                    navigationGutterIconBuilder.setTooltipText(mvcTooltip)
-                    navigationGutterIconBuilder.setAlignment(GutterIconRenderer.Alignment.CENTER)
-                    navigationGutterIconBuilder.setTargets(methods)
-                    result.add(navigationGutterIconBuilder.createLineMarkerInfo(element))
+                } else if (uClass.hasAnnotation(SpringAnnotations.REQUEST_MAPPING) && !uClass.hasModifierProperty(PsiModifier.ABSTRACT)) {
+                    val apiMarker = apiMethodService.getApiMarker(element.parent) ?: return
+                    val controllers = jvmFileExtensions.flatMap {
+                        FilenameIndex.getAllFilesByExt(project, it)
+                            .asSequence()
+                            .filter { f -> f.name.endsWith("Client.$it") }
+                            .mapNotNull { f -> f.toPsiFile(project) }
+                            .flatMap { f ->
+                                PsiTreeUtil.findChildrenOfAnyType(f, PsiClass::class.java, KtClass::class.java)
+                                    .filter { clazz ->
+                                        val toUElement = clazz.toUElement()
+                                        return@filter if (toUElement is UClass) {
+                                            return@filter toUElement.isInterface
+                                                    && toUElement.hasAnnotation(SpringAnnotations.FEIGN_CLIENT)
+                                                    && toUElement.name != null && toUElement.name!!.endsWith("Client")
+                                        } else false
+                                    }
+                                    .mapNotNull { f -> f.kotlinFqName?.asString() }
+                            }
+                            .mapNotNull { name -> psiService.findClass(name) }
+                    }
+
+                    val methods = controllers.stream().flatMap { c -> c.allMethods.stream() }
+                        .filter { method ->
+                            val marker = apiMethodService.getApiMarker(method)
+                            marker != null && marker.contains(apiMarker)
+                        }
+                        .toList()
+
+                    if (methods.isNotEmpty()) {
+                        val icon = AllIcons.Spring.CLOUD
+                        val navigationGutterIconBuilder: NavigationGutterIconBuilder<PsiElement> = NavigationGutterIconBuilder.create(icon)
+                        navigationGutterIconBuilder.setTooltipText(mvcTooltip)
+                        navigationGutterIconBuilder.setAlignment(GutterIconRenderer.Alignment.CENTER)
+                        navigationGutterIconBuilder.setTargets(methods)
+                        result.add(navigationGutterIconBuilder.createLineMarkerInfo(element))
+                    }
                 }
+
             }
-
+        } catch (ex: ProcessCanceledException) {
+            // ignore
         }
     }
 
