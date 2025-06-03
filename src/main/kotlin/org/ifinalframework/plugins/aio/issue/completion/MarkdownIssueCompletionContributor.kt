78e13239ed44a84ac65adda9b65182925bcbe5a5
iimik <iimik@qq.com>
2025-06-03 10:03:02 +0800

    versions: deploy 0.0.19

---
+++ src/main/kotlin/org/ifinalframework/plugins/aio/issue/completion/MarkdownIssueCompletionContributor.kt
@@ -0,0 +1,59 @@
+package org.ifinalframework.plugins.aio.issue.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PsiJavaPatterns
+import com.intellij.tasks.TaskManager
+import com.intellij.util.ProcessingContext
+import org.ifinalframework.plugins.aio.resource.AllIcons
+
+
+/**
+ * MarkdownIssueCompletionContributor
+ *
+ * @author iimik
+ * @since 0.0.19
+ */
+class MarkdownIssueCompletionContributor : CompletionContributor() {
+    init {
+        extend(
+            CompletionType.BASIC,
+            PsiJavaPatterns.psiElement(),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+                    val position = parameters.position
+                    val text = position.text
+                    if (text.startsWith("#")) {
+                        val project = position.project
+                        val taskManager = TaskManager.getManager(project)
+                        val tasks = taskManager.cachedIssues
+                        tasks?.forEach { issue ->
+                            var icon = issue.icon
+                            var tagName = "issue"
+                            if (issue.repository?.javaClass?.name?.contains("jira", ignoreCase = true) ?: false) {
+                                tagName = "jira"
+                                icon = AllIcons.Issues.JIRA
+                            }
+                            val summary = issue.summary
+                            val lookupString = "${issue.number}-${summary}"
+                            result.addElement(
+                                LookupElementBuilder.create(issue, lookupString)
+                                    .withIcon(icon)
+                                    .withCaseSensitivity(false)
+                                    .withInsertHandler { context, _ ->
+                                        val document = context.document
+                                        val offset = position.textOffset
+                                        val newSummary = issue.summary
+                                            .replace("[", "\\[")
+                                            .replace("]", "\\]")
+                                        val content = "[${issue.number}-${newSummary}](${issue.issueUrl})"
+                                        document.replaceString(offset, offset + lookupString.length + 1, content)
+                                    }
+                            )
+                        }
+                    }
+
+                }
+            })
+    }
+}
