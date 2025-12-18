f88c737771074415b3f6e83bb75dcb154d51548e
iimik <iimik@qq.com>
2025-12-18 16:27:00 +0800

    feature: markdown task

--- src/main/kotlin/org/ifinalframework/plugins/aio/tasks/markdown/MarkdownTaskCompletionContributor.kt
+++ src/main/kotlin/org/ifinalframework/plugins/aio/tasks/markdown/MarkdownTaskCompletionContributor.kt
@@ -1,11 +1,9 @@
 package org.ifinalframework.plugins.aio.tasks.markdown
 
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionProvider
-import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.jvm.dfa.analysis.ui.inspection.presentation.PsiElementLineLocator.getStartLine
+import com.intellij.openapi.util.TextRange
 import com.intellij.patterns.PsiJavaPatterns
 import com.intellij.tasks.TaskManager
 import com.intellij.util.ProcessingContext
@@ -31,58 +29,65 @@ class MarkdownTaskCompletionContributor : CompletionContributor() {
                 override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
                     val position = parameters.position
                     val text = position.text
-                    if (text.startsWith("@")) {
-                        val project = position.project
-                        val taskManager = TaskManager.getManager(project)
-                        val tasks = taskManager.cachedIssues
-                        tasks?.forEach { issue ->
-                            var icon = issue.icon
-                            var tagName = "issue"
-                            if (issue.repository?.javaClass?.name?.contains("jira", ignoreCase = true) ?: false) {
-                                tagName = "jira"
-                                icon = AllIcons.Task.JIRA
+                    position.getStartLine()
+                    val lineStartOffset = parameters.editor.document.getLineStartOffset(position.getStartLine() - 1)
+                    val line = parameters.editor.document.getText(TextRange(lineStartOffset, position.textOffset))
+                    val isHeader = line.startsWith("#")
+
+                    if (text.startsWith("@") || text.startsWith("#")) {
+                        if (isHeader) {
+                            val project = position.project
+                            val taskManager = TaskManager.getManager(project)
+                            val tasks = taskManager.cachedIssues
+                            tasks?.forEach { issue ->
+                                var icon = issue.icon
+                                var tagName = "issue"
+                                if (issue.repository?.javaClass?.name?.contains("jira", ignoreCase = true) ?: false) {
+                                    tagName = "jira"
+                                    icon = AllIcons.Task.JIRA
+                                }
+                                val summary = issue.summary
+                                val lookupString = "${issue.number}-${summary}"
+                                result.addElement(
+                                    LookupElementBuilder.create(issue, lookupString)
+                                        .withIcon(icon)
+                                        .withCaseSensitivity(false)
+                                        .withInsertHandler { context, _ ->
+                                            val document = context.document
+                                            val offset = position.textOffset
+                                            val newSummary = issue.summary
+                                                .replace("[", "\\[")
+                                                .replace("]", "\\]")
+                                            val content = "[${issue.number}-${newSummary}](${issue.issueUrl})"
+                                            document.replaceString(offset, offset + lookupString.length + 1, content)
+                                        }
+                                )
                             }
-                            val summary = issue.summary
-                            val lookupString = "${issue.number}-${summary}"
-                            result.addElement(
-                                LookupElementBuilder.create(issue, lookupString)
-                                    .withIcon(icon)
-                                    .withCaseSensitivity(false)
-                                    .withInsertHandler { context, _ ->
-                                        val document = context.document
-                                        val offset = position.textOffset
-                                        val newSummary = issue.summary
-                                            .replace("[", "\\[")
-                                            .replace("]", "\\]")
-                                        val content = "[${issue.number}-${newSummary}](${issue.issueUrl})"
-                                        document.replaceString(offset, offset + lookupString.length + 1, content)
-                                    }
-                            )
-                        }
-                    }else if(text.startsWith("#")) {
-                        val project = position.project
-                        val taskManager = TaskManager.getManager(project)
-                        val tasks = taskManager.cachedIssues
-                        tasks?.forEach { issue ->
-                            var icon = issue.icon
-                            var tagName = "issue"
-                            if (issue.repository?.javaClass?.name?.contains("jira", ignoreCase = true) ?: false) {
-                                tagName = "jira"
-                                icon = AllIcons.Task.JIRA
+                        } else {
+                            val project = position.project
+                            val taskManager = TaskManager.getManager(project)
+                            val tasks = taskManager.cachedIssues
+                            tasks?.forEach { issue ->
+                                var icon = issue.icon
+                                var tagName = "issue"
+                                if (issue.repository?.javaClass?.name?.contains("jira", ignoreCase = true) ?: false) {
+                                    tagName = "jira"
+                                    icon = AllIcons.Task.JIRA
+                                }
+                                val summary = issue.summary
+                                val lookupString = "${issue.number}-${summary}"
+                                result.addElement(
+                                    LookupElementBuilder.create(issue, lookupString)
+                                        .withIcon(icon)
+                                        .withCaseSensitivity(false)
+                                        .withInsertHandler { context, _ ->
+                                            val document = context.document
+                                            val offset = position.textOffset
+                                            val content = "[#${issue.id}](${issue.issueUrl})"
+                                            document.replaceString(offset, offset + lookupString.length + 1, content)
+                                        }
+                                )
                             }
-                            val summary = issue.summary
-                            val lookupString = "${issue.number}-${summary}"
-                            result.addElement(
-                                LookupElementBuilder.create(issue, lookupString)
-                                    .withIcon(icon)
-                                    .withCaseSensitivity(false)
-                                    .withInsertHandler { context, _ ->
-                                        val document = context.document
-                                        val offset = position.textOffset
-                                        val content = "[#${issue.id}](${issue.issueUrl})"
-                                        document.replaceString(offset, offset + lookupString.length + 1, content)
-                                    }
-                            )
                         }
                     }
 
